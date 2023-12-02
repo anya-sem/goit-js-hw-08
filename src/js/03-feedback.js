8f7f72b87eb905752d22c57be9483dfd57f3d144
anya-sem <139631738+anya-sem@users.noreply.github.com>
2023-12-02 15:24:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,61 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+
 const items = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('[name="message"]'),
   input: document.querySelector('[name="email"]'),
 };
 
+const dataForm = {};
+
 items.form.addEventListener('submit', onFormSubmit);
 items.form.addEventListener('input', throttle(onInput, 500));
 
-const dataForm = {};
+onReload();
 
 function onInput(event) {
-dataForm.email = items.input.value;
-  dataForm.message = items.textarea.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(dataForm));
+dataForm[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
 }
 
-const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-if (savedData) {
-  items.input.value = savedData.email;
-  items.textarea.value = savedData.message;
+  function onReload() {
+ const savedData = localStorage.getItem(STORAGE_KEY);
+   if (savedData) {
+    //  console.log(savedData);
+    const parsedData = JSON.parse(savedData);
+    // console.log(parsedData);
+   items.input.value = parsedData.email;
+   items.textarea.value = parsedData.message;
 }
-
-// function onReload() {
-//  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-//    if (savedData) {
-//   items.input.value = savedData.email;
-//   items.textarea.value = savedData.message;
-// }
-// }
+  }
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(dataForm);
-  items.form.reset();
-  localStorage.removeItem('feedback-form-state');
+  if (items.input.value === '' || items.textarea.value === '') {
+    return alert(`Please fill in all the fields`);
+  } else {
+    localStorage.removeItem(STORAGE_KEY);
+    dataForm.email = items.input.value;
+        dataForm.message = items.textarea.value;
+    event.currentTarget.reset();
+    console.log(dataForm);
+  }
+
+//  event.preventDefault();
+//   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+//   items.form.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+//   event.preventDefault();
+//     dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//     items.form.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//     console.log(dataForm);
+// }
 }
+
 
 
